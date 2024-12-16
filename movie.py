d83e88be3c6f7d5588841f837cff23356a2d7463
norkirie <racastro@up.edu.ph>
2024-12-16 20:26:46 +0800

    final edits

--- movie.py
+++ movie.py
@@ -1,14 +1,13 @@
 class Movie:
-    def __init__(self, title, genre, year, rating, comments, movieid=None):
+    def __init__(self, title, genre, year, rating, comments, movie_id=None):
         self.title = title
         self.genre = genre
         self.year = year
         self.rating = rating
         self.comments = comments
-        self.movie_id = movieid
+        self.movie_id = movie_id
 
     def __repr__(self):
-        return f"{self.title} ({self.year}) - {self.genre}"
-
+        return f"Movie({self.movie_id}, {self.title}, {self.year}, {self.rating}, {self.genre}, {self.comments})"
 
 
